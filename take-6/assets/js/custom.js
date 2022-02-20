891f3dd36656a0511cd0be602677fe74266ecc55
vaibhav-trial <joshi93vaibhav@gmail.com>
2022-02-20 23:21:07 +0530

    Update svg fill when changing color

--- take-6/assets/js/custom.js
+++ take-6/assets/js/custom.js
@@ -1,9 +1,14 @@
 (function () {
   const picker = document.querySelector('#color-picker');
   const els = document.querySelectorAll('.bg-custom-color');
+  const svgPaths = document.querySelectorAll('path');
   picker.addEventListener('change', function (e) {
+    const value = e.target.value
     els.forEach(function (el) {
-      el.style.backgroundColor = e.target.value;
+      el.style.backgroundColor = value;
     });
+    svgPaths.forEach(function (p) {
+      p.setAttribute('fill', value)
+    })
   });
 })();
